582f6270e523a06c4ca1524936aa9fd5cea07fab
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-21 23:29:18 +0700

    feat(main): add main program

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	internals_task1 "github.com/FebryanHernanda/minitask-day3-week8-koda3/internals/task1"
+	internals_task2 "github.com/FebryanHernanda/minitask-day3-week8-koda3/internals/task2"
+	internals_task3 "github.com/FebryanHernanda/minitask-day3-week8-koda3/internals/task3"
+)
 
+func main() {
+	/* Task 1 */
+	internals_task1.ShowTask1()
+
+	/* Task 2 */
+	internals_task2.ShowTask2()
+
+	/* Task 3 */
+	internals_task3.ShowTask3()
 }
